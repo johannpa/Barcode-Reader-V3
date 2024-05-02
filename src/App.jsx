ceff6de1a4d928e80b7c07bb073ca382eeabbf7c
johannpa <johann_paque@hotmail.fr>
2024-05-02 20:25:32 +0200

    The scan was always running but it's fix now

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,31 @@
-import { useState } from 'react'
-import { BarcodeScanner } from 'react-webcam-barcode-scanner'
+import { useState, useEffect } from 'react'
+import BarcodeScanner  from 'react-webcam-barcode-scanner'
 import './App.css'
 
 function App() {
   const [data, setData] = useState('No Barcode Scanned');
+  let logTimeout;
 
+  const handleUpdate = (err, result) => {
+    if (result) {
+      setData(result.text);
+      console.log('Barcode detected:', result.text);
+    } else {
+      clearTimeout(logTimeout);
+      logTimeout = setTimeout(() => {
+        console.log('No barcode detected');
+      }, 1000); 
+    }
+  };
+
+  useEffect(() => {
+    return () => clearTimeout(logTimeout);
+  }, []);
   return (
     <>
-      <div className="App" style={{ padding: "5rem 10rem" }}>
+       <div className="App" style={{ padding: "5rem 10rem" }}>
       <BarcodeScanner
-        onUpdate={(err, result) => {
-          if (result) setData(result.text);
-          else setData('No Barcode Scanned');
-        }}
+        onUpdate={handleUpdate}
         width={500}
         height={500}
       />
