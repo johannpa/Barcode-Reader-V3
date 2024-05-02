a05e56c5be0831746378006f392bb1218baba506
johannpa <johann_paque@hotmail.fr>
2024-05-02 20:05:44 +0200

    Main part of the application

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BarcodeScanner } from 'react-webcam-barcode-scanner'
 import './App.css'
 
 function App() {
-  
+  const [data, setData] = useState('No Barcode Scanned');
+
   return (
     <>
-      <h1>Barcode Reader Version 3 TEST</h1>
+      <div className="App" style={{ padding: "5rem 10rem" }}>
+      <BarcodeScanner
+        onUpdate={(err, result) => {
+          if (result) setData(result.text);
+          else setData('No Barcode Scanned');
+        }}
+        width={500}
+        height={500}
+      />
+      <p>Barcode: {data}</p>
+    </div>
+  );
     </>
   )
 }
